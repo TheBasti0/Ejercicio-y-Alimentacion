12cfc06a79e6035679e84ea860c7bcd5d8c2b864
basti <bastianurrutia2000@gmail.com>
2020-09-02 01:45:30 -0400

    Segundo Commit: se agregan widget y se generan las referencias desde java

--- app/src/main/java/com/urrutia/ejercicio_y_alimentacion/MainActivity.java
+++ app/src/main/java/com/urrutia/ejercicio_y_alimentacion/MainActivity.java
@@ -2,13 +2,27 @@ package com.urrutia.ejercicio_y_alimentacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Creamos la variables que haran referencia a los widget
+    private TextView txtTitulo;
+    private Button btnIniciar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Realizar las referencias
+        txtTitulo = findViewById(R.id.txtTitulo);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+
     }
 }
